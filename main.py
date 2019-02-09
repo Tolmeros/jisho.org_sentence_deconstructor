466a52cec07623fc28926c2147ff6059a18481d6
tolmeros@phoenix <pass>
2019-02-09 21:53:26 +0200

    Basic beta functionality.

---
+++ main.py
@@ -0,0 +1,94 @@
+import argparse
+import re
+from urllib.request import urlopen
+
+from bs4 import BeautifulSoup
+
+
+jisho_org_sentences = re.compile(
+    r'^https?://jisho\.org/sentences/[A-Z0-9]+/?$', re.IGNORECASE)
+
+
+def check_url(value):
+    if not test_url(value):
+        raise argparse.ArgumentTypeError("%s is an invalid url." % value)
+    return value
+
+
+def test_url(url):
+    return re.match(jisho_org_sentences, url) is not None
+
+def get_page(url):
+    return urlopen(url)
+
+def get_sentence(html):
+    #<div class="sentence_content">
+    soup = BeautifulSoup(html, 'html.parser')
+    return soup.find('div', attrs={'class':'sentence_content'})
+
+def process_sentence(sentence):
+    def ja(ul_tag):
+        data = {
+            'kana':[],
+            'mixed':[],
+        }
+
+        for tag in ul_tag.find_all('li', recursive=False):
+            spans = {x.attrs['class'][0]:x for x in tag.find_all('span')}
+            if 'furigana' in spans:
+                data['kana'].append(spans['furigana'].text)
+            else:
+                data['kana'].append(spans['unlinked'].text)
+
+            data['mixed'].append(spans['unlinked'].text)
+        return data
+
+
+    #analyse
+    collected_data = {}
+    tags = []
+
+    for tag in sentence.find_all(recursive=False):
+        tags.append({'tag':tag.name, 'attrs': tag.attrs})
+        if tag.name == 'div':
+            if 'english_sentence' in tag.attrs['class']:
+                #spans = tag.find_all('span', recursive=False)
+                spans = { span.attrs['class'][0]: span for span in tag.find_all('span', recursive=False)}
+                collected_data['english'] = {
+                    'text': spans['english'].text,
+                    'source': spans['inline_copyright'].find('a').attrs['href'],
+                }
+
+
+        elif tag.name == 'ul':
+            if 'japanese_sentence' in tag.attrs['class']:
+                collected_data['japanese'] = ja(tag)
+
+
+
+    return collected_data
+
+
+def make_text(data):
+    s = ''
+    s += '{}\n'.format(''.join(data['japanese']['mixed']))
+    s += '{}\n'.format(' '.join(data['japanese']['kana']))
+    s += '{}\n'.format(''.join(data['english']['text']))
+    s += '\n{}\n'.format(''.join(data['english']['source']))
+    return s
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url', type=check_url)
+    args = parser.parse_args()
+
+    page = get_page(args.url)
+    sentence = get_sentence(page)
+
+    text = make_text(process_sentence(sentence))
+    print(text)
+
+
+if __name__ == '__main__':
+    main()
